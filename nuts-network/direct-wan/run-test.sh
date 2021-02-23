53fc6c294b1cb58f24e22b780fef008e375839f3
Rein Krul <info@reinkrul.nl>
2021-02-23 10:55:33 +0100

    Fixes network tests for new Nuts Node

--- nuts-network/direct-wan/run-test.sh
+++ nuts-network/direct-wan/run-test.sh
@@ -7,39 +7,11 @@ echo "Cleaning up running Docker containers and volumes, and key material..."
 echo "------------------------------------"
 docker-compose down
 docker-compose rm -f -v
-rm -rf ./node-*/data
-mkdir -p ./node-A/data/keys
-mkdir -p ./node-B/data/keys
 
 echo "------------------------------------"
 echo "Starting Docker containers..."
 echo "------------------------------------"
 docker-compose up -d
-waitForDCService nodeA
-waitForDCService nodeB
-
-echo "------------------------------------"
-echo "Registering vendors..."
-echo "------------------------------------"
-# Register Vendor A
-docker-compose exec -e NUTS_MODE=cli nodeA nuts crypto selfsign-vendor-cert "Vendor A" /opt/nuts/keys/vendor_certificate.pem
-docker-compose exec -e NUTS_MODE=cli nodeA nuts registry register-vendor /opt/nuts/keys/vendor_certificate.pem
-
-# Register Vendor B
-docker-compose exec -e NUTS_MODE=cli nodeB nuts crypto selfsign-vendor-cert "Vendor B" /opt/nuts/keys/vendor_certificate.pem
-docker-compose exec -e NUTS_MODE=cli nodeB nuts registry register-vendor /opt/nuts/keys/vendor_certificate.pem
-
-# Since node B connects to A's gRPC server, so A needs to trust B's Vendor CA certificate since it's used to issue the client certificate
-cat ./node-B/data/keys/vendor_certificate.pem >> ./node-A/data/keys/truststore.pem
-# This also means that B must trust A's server certificate (by trusting our custom Root CA)
-docker cp ../../keys/ca-certificate.pem $(docker-compose ps -q nodeB):/usr/local/share/ca-certificates/rootca.crt
-docker-compose exec nodeB update-ca-certificates
-
-docker-compose restart
-
-echo "------------------------------------"
-echo "Waiting for services to restart..."
-echo "------------------------------------"
 waitForDCService nodeA
 waitForDCService nodeB
 
