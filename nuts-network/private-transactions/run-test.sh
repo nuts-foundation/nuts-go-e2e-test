26b46640f62705dd826eae95d509f55ec00183be
Rein Krul <info@reinkrul.nl>
2022-08-24 07:43:47 +0200

    Add VC revoke to private TX test

--- nuts-network/private-transactions/run-test.sh
+++ nuts-network/private-transactions/run-test.sh
@@ -22,7 +22,11 @@ function createAuthCredential() {
       "subject": "urn:oid:2.16.840.1.113883.2.4.6.3:123456780"
     },
    "visibility": "private"
-  }' "$2" "$3" | curl -s -X POST "$1/internal/vcr/v2/issuer/vc" -H "Content-Type: application/json" --data-binary @- > /dev/null
+  }' "$2" "$3" | curl -s -X POST "$1/internal/vcr/v2/issuer/vc" -H "Content-Type: application/json" --data-binary @- | jq ".id" | sed "s/\"//g"
+}
+
+function revokeAuthCredential() {
+  curl -s -X DELETE "$1/internal/vcr/v2/issuer/vc/$2" > /dev/null
 }
 
 function searchAuthCredentials() {
@@ -56,13 +60,15 @@ echo "Asserting..."
 echo "------------------------------------"
 
 didNodeA=$(findNodeDID "node-A/nuts.yaml")
-printf "NodeDID for node-a: %s\n" "$didNodeA"
+printf "NodeDID for node A: %s\n" "$didNodeA"
 
 didNodeB=$(findNodeDID "node-B/nuts.yaml")
-printf "NodeDID for node-b: %s\n" "$didNodeB"
+printf "NodeDID for node B: %s\n" "$didNodeB"
 
-createAuthCredential "http://localhost:11323" "$didNodeA" "$didNodeB"
-createAuthCredential "http://localhost:21323" "$didNodeB" "$didNodeA"
+vcNodeA=$(createAuthCredential "http://localhost:11323" "$didNodeA" "$didNodeB")
+printf "VC issued by node A: %s\n" "$vcNodeA"
+vcNodeB=$(createAuthCredential "http://localhost:21323" "$didNodeB" "$didNodeA")
+printf "VC issued by node B: %s\n" "$vcNodeB"
 
 # Wait for transactions to sync
 sleep 10
@@ -74,6 +80,25 @@ fi
 
 if [ $(searchAuthCredentials "http://localhost:21323" | jq ".verifiableCredentials[].verifiableCredential.id" | wc -l) -ne "2" ]; then
   echo "failed to find NutsAuthorizationCredentials on Node-B"
+  exitWithDockerLogs 1
+fi
+
+echo "------------------------------------"
+echo "Revoking NutsAuthorizationCredential..."
+echo "------------------------------------"
+revokeAuthCredential "http://localhost:11323" "${vcNodeA//#/%23}"
+revokeAuthCredential "http://localhost:21323" "${vcNodeB//#/%23}"
+
+# Wait for transactions to sync
+sleep 5
+
+if [ $(searchAuthCredentials "http://localhost:11323" | jq ".verifiableCredentials[].verifiableCredential.id" | wc -l) -ne "0" ]; then
+  echo "NutsAuthorizationCredentials should have been revoked so they can't be resolved on Node-A"
+  exitWithDockerLogs 1
+fi
+
+if [ $(searchAuthCredentials "http://localhost:21323" | jq ".verifiableCredentials[].verifiableCredential.id" | wc -l) -ne "0" ]; then
+  echo "NutsAuthorizationCredentials should have been revoked so they can't be resolved on Node-A"
   exitWithDockerLogs 1
 fi
 
