5bb9bccb9e2816123cdd9c29b4c95566391e1dea
Rein Krul <info@reinkrul.nl>
2023-05-31 15:17:29 +0200

    set -e

--- auth/selfsigned/run-test.sh
+++ auth/selfsigned/run-test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source ../../util.sh
 
+set -e # make script fail if any of the tests returns a non-zero exit code
+
 # Shut down existing containers
 docker compose stop
 docker compose rm -f -v
